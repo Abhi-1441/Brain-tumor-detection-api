3f69c783b948d1e5afb6181d753f244384b2da51
Abhi-1441 <pakhmodeabhishek1441@gmail.com>
2024-04-07 23:30:41 +0530

    Add prediction directory and change prediction file name

--- index.py
+++ index.py
@@ -11,14 +11,14 @@ CORS(app)
 # Load YOLOv8m model
 yolo_model = YOLO('./brain_tumor_adamax.pt')
 
-def predict(image_data):
+def predict(image_data, filename):
     image = Image.open(io.BytesIO(image_data))
     results = yolo_model(image)
     save_dir = './predictions'
     os.makedirs(save_dir, exist_ok=True)
     
     for i, result in enumerate(results):
-        prediction_path = os.path.join(save_dir, f'prediction.png')
+        prediction_path = os.path.join(save_dir, f'{filename}_prediction.png')
         result.save(prediction_path)
     
     return prediction_path
@@ -33,8 +33,10 @@ def predict_image():
         return jsonify({'error': 'No selected file'})
 
     if file:
-        prediction_path = predict(file.read())
+        filename = os.path.splitext(file.filename)[0]  # Extract filename without extension
+        prediction_path = predict(file.read(), filename)
         return send_file(prediction_path)
 
 if __name__ == '__main__':
     app.run(debug=True)
+    
