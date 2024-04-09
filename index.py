cd013404046b48787a1fc926afaa0cd1ad67467e
Abhi-1441 <pakhmodeabhishek1441@gmail.com>
2024-04-09 13:08:38 +0530

    update Flask[async] in requirements

--- index.py
+++ index.py
@@ -4,6 +4,7 @@ from flask import Flask, request, jsonify, send_file
 from flask_cors import CORS
 from PIL import Image
 from ultralytics import YOLO
+import asyncio
 
 app = Flask(__name__)
 CORS(app)
